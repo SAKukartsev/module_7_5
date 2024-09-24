10e48e62988963b2ad4743a1f6a7fb17a3857932
SAKukartsev <sergcover@yandex.ru>
2024-09-24 22:15:55 +0700

    Update module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -5,7 +5,7 @@ directory = r'D:\Pyton_project\Serg\datas\7mod'
 
 for root, dirs, files in os.walk(directory):
   for file in files:
-    filepath = os.path.join(str(directory))
+    filepath = os.path.join(root, file)
     filetime = os.path.getmtime(file)
     formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
     filesize = os.path.getsize(file)
